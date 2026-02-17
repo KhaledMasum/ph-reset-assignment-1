37d82c48ad6899142228a9384f331a00cb631e89
Khaled Masum <khaledmasum1@gmail.com>
2026-02-17 23:37:33 +0600

    Make Products & Categories dynamic

---
+++ script.js
@@ -0,0 +1,142 @@
+// Load All Categories
+function loadProductCategories() {
+  fetch("https://fakestoreapi.com/products/categories")
+    .then((res) => res.json())
+    .then((data) => {
+      const categories = data;
+
+      showCategories(categories);
+    });
+}
+
+function showCategories(categories) {
+  const categoryContainer = document.getElementById("category-container");
+
+  categories.forEach((category) => {
+    const buttonDiv = document.createElement("div");
+
+    const button = document.createElement("button");
+    button.className = "btn btn-primary btn-outline btn-sm";
+    button.textContent = category;
+
+    button.addEventListener("click", function () {
+      loadCategorizedProducts(category);
+    });
+
+    buttonDiv.appendChild(button);
+    categoryContainer.appendChild(buttonDiv);
+  });
+}
+
+// Load All Products
+function loadAllProducts() {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+      const products = data;
+
+      showProducts(products);
+    });
+}
+
+function showProducts(products) {
+  const productContainer = document.getElementById("products-container");
+
+  products.forEach((product) => {
+    console.log(products);
+
+    const productDiv = document.createElement("div");
+    productDiv.innerHTML = `
+          <div class="card bg-base-100 shadow-sm">
+            <figure class="h-40 flex items-center justify-center">
+                <img
+                    src="${product.image}"
+                    class="h-full object-contain"
+                />
+            </figure>
+              <div class="p-4">
+                  <div class="flex justify-between items-center">
+                    <div class="badge badge-primary">
+                        ${product.category}
+                    </div>
+
+                    <div>
+                        <i class="fa-solid fa-star text-amber-600"></i>
+                        ${product.rating.rate} (${product.rating.count})
+                    </div>
+                  </div>
+
+                  <h2 class="card-title line-clamp-2">${product.title}</h2>
+                  <p class="line-clamp-3">${product.description}</p>
+
+                  <h2 class="text-2xl font-bold">$${product.price}</h2>
+
+                  <div class="flex gap-2 mt-3">
+                  <button class="btn btn-primary btn-outline btn-sm flex-1">
+                      Buy Now
+                  </button>
+                  <button class="btn btn-primary btn-sm flex-1">Buy Now</button>
+                  </div>
+              </div>
+          </div>
+      `;
+
+    productContainer.appendChild(productDiv);
+  });
+}
+
+function loadCategorizedProducts(category) {
+  fetch(`https://fakestoreapi.com/products/category/${category}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const products = data;
+
+      showCategorizedProducts(products);
+    });
+}
+
+function showCategorizedProducts(products) {
+  const productContainer = document.getElementById("products-container");
+  productContainer.innerHTML = "";
+
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.innerHTML = `
+          <div class="card bg-base-100 shadow-sm">
+            <figure class="h-40 flex items-center justify-center">
+                <img
+                    src="${product.image}"
+                    class="h-full object-contain"
+                />
+            </figure>
+              <div class="p-4">
+                  <div class="flex justify-between items-center">
+                    <div class="badge badge-primary">
+                        ${product.category}
+                    </div>
+
+                    <div>
+                        <i class="fa-solid fa-star text-amber-600"></i>
+                        ${product.rating.rate} (${product.rating.count})
+                    </div>
+                  </div>
+
+                  <h2 class="card-title truncate">${product.title}</h2>
+                  <p class="line-clamp-3">${product.description}</p>
+
+                  <div class="flex gap-2 mt-3">
+                  <button class="btn btn-primary btn-outline btn-sm flex-1">
+                      Buy Now
+                  </button>
+                  <button class="btn btn-primary btn-sm flex-1">Buy Now</button>
+                  </div>
+              </div>
+          </div>
+      `;
+
+    productContainer.appendChild(productDiv);
+  });
+}
+
+loadProductCategories();
+loadAllProducts();
