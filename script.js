ebe850f7f908f1c6b50069e65621ef9b24e135fe
Khaled Masum <khaledmasum1@gmail.com>
2026-03-01 00:18:11 +0600

    Implemented some remaining functionalities also updated the readme file for resubmitting the assignment

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// Load All Categories
-function loadProductCategories() {
-  fetch("https://fakestoreapi.com/products/categories")
-    .then((res) => res.json())
-    .then((data) => {
-      const categories = data;
-
-      showCategories(categories);
-    });
-}
-
-function showCategories(categories) {
-  const categoryContainer = document.getElementById("category-container");
-
-  categories.forEach((category) => {
-    const buttonDiv = document.createElement("div");
-
-    const button = document.createElement("button");
-    button.className = "btn btn-primary btn-outline btn-sm";
-    button.textContent = category;
-
-    button.addEventListener("click", function () {
-      loadCategorizedProducts(category);
-    });
-
-    buttonDiv.appendChild(button);
-    categoryContainer.appendChild(buttonDiv);
-  });
-}
-
 // Load All Products
 function loadAllProducts() {
   fetch("https://fakestoreapi.com/products")
@@ -35,16 +5,15 @@ function loadAllProducts() {
     .then((data) => {
       const products = data;
 
-      showProducts(products);
+      showTrandingProducts(products);
     });
 }
 
-function showProducts(products) {
-  const productContainer = document.getElementById("products-container");
+// Show trending products
+function showTrandingProducts(products) {
+  const trandingContainer = document.getElementById("tranding-container");
 
-  products.forEach((product) => {
-    console.log(products);
-
+  products.slice(0, 3).forEach((product) => {
     const productDiv = document.createElement("div");
     productDiv.innerHTML = `
           <div class="card bg-base-100 shadow-sm">
@@ -68,75 +37,50 @@ function showProducts(products) {
 
                   <h2 class="card-title line-clamp-2">${product.title}</h2>
                   <p class="line-clamp-3">${product.description}</p>
-
                   <h2 class="text-2xl font-bold">$${product.price}</h2>
 
                   <div class="flex gap-2 mt-3">
-                  <button class="btn btn-primary btn-outline btn-sm flex-1">
-                      Buy Now
+                  <button class="btn btn-primary btn-outline btn-sm flex-1" onclick="loadProductDetails(${product.id})">
+                      Details
                   </button>
-                  <button class="btn btn-primary btn-sm flex-1">Buy Now</button>
+                  <button class="btn btn-primary btn-sm flex-1">Add to Cart</button>
                   </div>
               </div>
           </div>
       `;
 
-    productContainer.appendChild(productDiv);
+    trandingContainer.appendChild(productDiv);
   });
 }
 
-function loadCategorizedProducts(category) {
-  fetch(`https://fakestoreapi.com/products/category/${category}`)
+// Product details modal
+function loadProductDetails(id) {
+  fetch(`https://fakestoreapi.com/products/${id}`)
     .then((res) => res.json())
     .then((data) => {
-      const products = data;
-
-      showCategorizedProducts(products);
+      handleProductModal(data);
     });
 }
 
-function showCategorizedProducts(products) {
-  const productContainer = document.getElementById("products-container");
-  productContainer.innerHTML = "";
+const handleProductModal = (product) => {
+  const productDetailsContainer = document.getElementById(
+    "trending-product-container",
+  );
 
-  products.forEach((product) => {
-    const productDiv = document.createElement("div");
-    productDiv.innerHTML = `
-          <div class="card bg-base-100 shadow-sm">
-            <figure class="h-40 flex items-center justify-center">
-                <img
-                    src="${product.image}"
-                    class="h-full object-contain"
-                />
-            </figure>
-              <div class="p-4">
-                  <div class="flex justify-between items-center">
-                    <div class="badge badge-primary">
-                        ${product.category}
-                    </div>
+  productDetailsContainer.innerHTML = `
+    <h2 class="font-bold text-2xl">${product.title ? product.title : "No title found"}</h2>
+    <p>${product.description ? product.description : "No description found"}</p>
+    <div class="flex justify-between items-center">
+      <h4 class="text-2xl font-semibold">Price: $${product.price ? product.price : "No price found"}</h4>
+      <div>
+          <i class="fa-solid fa-star text-amber-600"></i>
+          ${product.rating.rate}
+      </div>
+    </div>
+    <button class="btn btn-primary btn-sm flex-1">Buy Now</button>
+  `;
 
-                    <div>
-                        <i class="fa-solid fa-star text-amber-600"></i>
-                        ${product.rating.rate} (${product.rating.count})
-                    </div>
-                  </div>
+  document.getElementById("trending_product_modal").showModal();
+};
 
-                  <h2 class="card-title truncate">${product.title}</h2>
-                  <p class="line-clamp-3">${product.description}</p>
-
-                  <div class="flex gap-2 mt-3">
-                  <button class="btn btn-primary btn-outline btn-sm flex-1">
-                      Buy Now
-                  </button>
-                  <button class="btn btn-primary btn-sm flex-1">Buy Now</button>
-                  </div>
-              </div>
-          </div>
-      `;
-
-    productContainer.appendChild(productDiv);
-  });
-}
-
-loadProductCategories();
 loadAllProducts();
